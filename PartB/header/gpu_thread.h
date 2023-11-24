8f497d42e3350f222ea259c50d30ae00ba965db6
Shweta Shukla <40588060+Shweta0002@users.noreply.github.com>
2023-11-24 21:45:43 +0530

    Update gpu_thread.h

--- PartB/header/gpu_thread.h
+++ PartB/header/gpu_thread.h
@@ -4,86 +4,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <ctime>
-#include <chrono>
-#include <fstream>
-#include <unistd.h>
 #include <string.h>
 using namespace std;
 
 #define THREADS_PER_BLOCK 16
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
 {
-   if (code != cudaSuccess) 
-   {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }
+    if (code != cudaSuccess)
+    {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
 }
 __global__ void dilatedConvolutionKernel(int input_row,
-                                         int input_col,
-                                         int *input,
-                                         int kernel_row,
-                                         int kernel_col,
-                                         int *kernel,
-                                         int output_row,
-                                         int output_col,
-                                         long long unsigned int *output)
+    int input_col,
+    int* input,
+    int kernel_row,
+    int kernel_col,
+    int* kernel,
+    int output_row,
+    int output_col,
+    long long unsigned int* output)
 {
+
+
+
     int output_i = blockIdx.y * blockDim.y + threadIdx.y;
     int output_j = blockIdx.x * blockDim.x + threadIdx.x;
-  
-
 
     if (output_i < output_row && output_j < output_col)
     {
-         output[output_i * output_col + output_j] = 0;
-       for(int kernel_i = 0; kernel_i< kernel_row; kernel_i++)
+        for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
+        {
+            for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
             {
-                for(int kernel_j = 0; kernel_j< kernel_col; kernel_j++)
-                {
-                    int input_i = (output_i + 2*kernel_i) % input_row;
-                    int input_j = (output_j + 2*kernel_j) % input_col;
-                    output[output_i * output_col + output_j] += input[input_i*input_col +input_j] 
-                                                                * kernel[kernel_i*kernel_col +kernel_j];
-                }
+                int input_i = (output_i + 2 * kernel_i) % input_row;
+                int input_j = (output_j + 2 * kernel_j) % input_col;
+                output[output_i * output_col + output_j] += input[input_i * input_col + input_j]
+                    * kernel[kernel_i * kernel_col + kernel_j];
             }
+        }
     }
 }
 
 void gpuThread(int input_row,
-               int input_col,
-               int *input,
-               int kernel_row,
-               int kernel_col,
-               int *kernel,
-               int output_row,
-               int output_col,
-               long long unsigned int *output)
+    int input_col,
+    int* input,
+    int kernel_row,
+    int kernel_col,
+    int* kernel,
+    int output_row,
+    int output_col,
+    long long unsigned int* output)
 {
-    int *d_input, *d_kernel;
-    long long unsigned int *d_output;
-    gpuErrchk(cudaMalloc((void **)&d_input, sizeof(int) * input_row * input_col));
-    gpuErrchk(cudaMalloc((void **)&d_kernel, sizeof(int) * kernel_row * kernel_col));
-    gpuErrchk(cudaMalloc((void **)&d_output, sizeof(long long unsigned int) * output_row * output_col));
+    // Allocate memory on the device
+    int* deviceInput, * deviceKernel;
+    long long unsigned int* deviceOutput;
 
-    std::cout << "hello" << std::endl;
-    // Copy input and kernel to device
-    gpuErrchk(cudaMemcpy(d_input, input, sizeof(int) * input_row * input_col, cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_kernel, kernel, sizeof(int) * kernel_row * kernel_col, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMalloc((void**)&deviceInput, sizeof(int) * input_row * input_col));
+    gpuErrchk(cudaMalloc((void**)&deviceKernel, sizeof(int) * kernel_row * kernel_col));
+    gpuErrchk(cudaMalloc((void**)&deviceOutput, sizeof(long long unsigned int) * output_row * output_col));
 
-    // Launch kernel
-    dim3 blockDim(2, 2);
-    dim3 gridDim((output_row + blockDim.x - 1) / blockDim.x, (output_col + blockDim.y - 1) / blockDim.y);
+    // Copy input and kernel matrices from host to device
+    gpuErrchk(cudaMemcpy(deviceInput, input, sizeof(int) * input_row * input_col, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(deviceKernel, kernel, sizeof(int) * kernel_row * kernel_col, cudaMemcpyHostToDevice));
 
-    dilatedConvolutionKernel<<<gridDim, blockDim>>>(input_row, input_col, d_input, kernel_row, kernel_col, d_kernel, output_row, output_col, d_output);
+    // Define block and grid dimensions
+    dim3 blockSize(32, 32);
+    dim3 gridSize((output_col + blockSize.x - 1) / blockSize.x, (output_row + blockSize.y - 1) / blockSize.y);
 
-    // Copy result back to host
-    gpuErrchk(cudaMemcpy(output, d_output, sizeof(int) * output_row * output_col, cudaMemcpyDeviceToHost));
+    // Launch the CUDA kernel
+    dilatedConvolutionKernel << <gridSize, blockSize >> > (input_row, input_col, deviceInput, kernel_row, kernel_col, deviceKernel, output_row, output_col, deviceOutput);
 
-    // Free device memory
-    gpuErrchk(cudaFree(d_input));
-    gpuErrchk(cudaFree(d_kernel));
-    gpuErrchk(cudaFree(d_output));
+    // Copy the result from device to host
+    gpuErrchk(cudaMemcpy(output, deviceOutput, sizeof(long long unsigned int) * output_row * output_col, cudaMemcpyDeviceToHost));
+
+    // Free allocated memory on the device
+    gpuErrchk(cudaFree(deviceInput));
+    gpuErrchk(cudaFree(deviceKernel));
+    gpuErrchk(cudaFree(deviceOutput));
+
+    
 }
